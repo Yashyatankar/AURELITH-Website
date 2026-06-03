b5635eb1d1f396d15b3474c804f2b6f0a2f2e379
Yash Patankar <yashrpatankar24@gmail.com>
2026-06-04 03:32:46 +0530

    Adding Cards

--- AURELITH/src/main.jsx
+++ AURELITH/src/main.jsx
@@ -4,13 +4,11 @@ import './index.css'
 import ScrollVideo from './assets/Components/ScrollVideo';
 import App from './App.jsx'
 import BentoBox from './assets/Components/BentoBox';
-import Collection from './assets/Components/Collection';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <ScrollVideo />
     <BentoBox />
-    <Collection />
   </StrictMode>,
 )
